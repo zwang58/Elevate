9b63809c0f0e3928b7aa498277bd658e988f6903
sippun <sippun@users.noreply.github.com>
2017-10-21 17:02:16 -0700

    Initial commit, show list with arbitrary items

---
+++ app/src/main/java/com/example/android/elevate/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.elevate;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.fragment_container) != null) {
+            getSupportFragmentManager().beginTransaction().
+                    add(R.id.fragment_container, new ToDoFragment()).commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/elevate/ToDoFragment.java
@@ -0,0 +1,36 @@
+package com.example.android.elevate;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Joel on 10/20/2017.
+ */
+
+public class ToDoFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] myDataset = {"Eat", "Sleep", "Code", "Repeat"};
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_todo, container, false);
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.my_recycler_view);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+        return rootView;
+    }
+
+}
+
+
