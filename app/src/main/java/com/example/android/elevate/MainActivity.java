6e71c4bff2e2213d4fb8fa1b3fcf35150d912f94
sippun <sippun@users.noreply.github.com>
2017-10-22 23:50:46 -0700

    Add Toolbar and switching between mood and todo fragments

--- app/src/main/java/com/example/android/elevate/MainActivity.java
+++ app/src/main/java/com/example/android/elevate/MainActivity.java
@@ -1,18 +1,41 @@
 package com.example.android.elevate;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
-        if (findViewById(R.id.fragment_container) != null) {
+        getSupportFragmentManager().beginTransaction().
+                add(R.id.fragment_container, new ToDoFragment()).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_items, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_mood) {
             getSupportFragmentManager().beginTransaction().
-                    add(R.id.fragment_container, new MoodFragment()).commit();
+                    replace(R.id.fragment_container, new MoodFragment()).commit();
         }
+        if (id == R.id.action_todo) {
+            getSupportFragmentManager().beginTransaction().
+                    replace(R.id.fragment_container, new ToDoFragment()).commit();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
